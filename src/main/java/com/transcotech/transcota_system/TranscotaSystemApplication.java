6dc98a70dd851c6787fec5245fc7080cd5fc5680
julianbayona <julianbayona0315@gmail.com>
2025-03-14 00:36:33 -0500

    first-commit

---
+++ src/main/java/com/transcotech/transcota_system/TranscotaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.transcotech.transcota_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscotaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscotaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transcotech/transcota_system/TranscotaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transcotech.transcota_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscotaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
