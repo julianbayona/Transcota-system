232f18b8fe6da34cc4833298c5d79bdfff6ed11f
julianbayona <julianbayona0315@gmail.com>
2025-03-15 13:36:03 -0500

    fix: complete endpoint

--- src/main/java/com/transcotech/transcota_system/controller/DriverController.java
+++ src/main/java/com/transcotech/transcota_system/controller/DriverController.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.transcotech.transcota_system.Service.DriverService;
+import com.transcotech.transcota_system.model.User;
 
 @Controller
 @RequestMapping("/api/drivers")
@@ -23,9 +26,14 @@ public class DriverController {
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteDriver(@PathVariable int id){
+    public String deleteDriver(@PathVariable Long id){
         driverService.deleteUser(id);
         return "";
+    }
+
+    @PostMapping("create")
+    public User createDriver(@RequestBody User user){
+        return driverService.createDriver(user);
     }
 
     
