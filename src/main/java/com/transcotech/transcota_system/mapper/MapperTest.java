18348dcbcc212b270af9e644bfc1b3134c00f6a6
Edison <edison.gutierrez01@uptc.edu.co>
2025-03-16 00:44:13 -0500

    feat: AddedDTO and Mapper classes

---
+++ src/main/java/com/transcotech/transcota_system/mapper/MapperTest.java
@@ -0,0 +1,12 @@
+package com.transcotech.transcota_system.mapper;
+
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@ApplicationScope
+public class MapperTest {
+
+    
+    public void testApp(){
+
+    }
+}
