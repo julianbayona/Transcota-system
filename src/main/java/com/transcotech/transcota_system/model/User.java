31a4c47b01b43faad0d9e1b412330656cc55611e
Edison <edison.gutierrez01@uptc.edu.co>
2025-03-18 12:16:56 -0500

    fix: fixed enum sql expresion

--- src/main/java/com/transcotech/transcota_system/model/User.java
+++ src/main/java/com/transcotech/transcota_system/model/User.java
@@ -2,15 +2,13 @@ package com.transcotech.transcota_system.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="userSystem")
 public class User extends Person {
 
-    @Enumerated(EnumType.STRING)
+    
     @Column(name= "role")
     private String role;
 
